c439d0a0336625fda8ee0b9e8051a3199514ea64
Guilherme Massini <guimassinifai@hotmail.com>
2020-03-24 16:02:40 -0300

    Primeiro Commit

---
+++ ClinicaDeAnimais/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+        Main app = new Main();
+        app.Start();
+
+	}
+
+	private void Start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
